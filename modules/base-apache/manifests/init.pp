57d39553a687eca1b4d2a4b0454d979c4b04b58f
David Eckelkamp <davide@homeaway.com>
2015-04-05 18:54:52 -0500

    initial checkin for sorry-server configs

---
+++ modules/base-apache/manifests/init.pp
@@ -0,0 +1,25 @@
+# Class: base-apache
+#
+# This class sets up the base configuration for the apache
+# web server on a host.  It should allow other modules to set virtual
+# hosts.
+#
+# Parameters:
+#
+# Actions:
+# - includes the 'apache' class. This should setup the base
+#   http config for the host.  
+#
+# Requires:
+#
+# Sample Usage:
+#
+class base-apache {
+  class { 'apache':
+    # Allow us to see logs without sudo
+    logroot_mode => '0755',
+    # Make Apache tell less about itself
+    server_tokens => 'ProductOnly',
+    log_formats   => { ha_logs => '%v %h %l %u %t \"%r\" %>s %b' }
+  }
+}
